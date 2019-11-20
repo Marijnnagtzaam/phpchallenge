e46afff5c92ba782f691d1f5976a64c0badef267
marijn <nagtzaammarijn@gmail.com>
2019-11-20 14:27:39 +0100

    added ability to view tasks

--- index.php
+++ index.php
@@ -34,6 +34,31 @@
     <?php } ?>
 		<input type="text" name="task" class="task_input">
 		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
-	</form>
+    </form>
+    <table>
+	<thead>
+		<tr>
+			<th>N</th>
+			<th>Tasks</th>
+			<th>Action</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php 
+		// select all tasks if page is visited or refreshed
+		$tasks = mysqli_query($db, "SELECT * FROM tasks");
+
+		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td class="task"> <?php echo $row['task']; ?> </td>
+				<td class="delete"> 
+					<a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+				</td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+</table>
 </body>
 </html>
